e4c1d4018ce18cd5ed8bfee3cbbbc4fea3416f3c
wildrun0 <gamesnouth@yandex.ru>
2022-12-06 20:02:25 +0500

    lightmode design improves & Worlds bugfix cwap unavailable in <World> comp fixed <World> was moved from <Worlds> comp White colors in bg in lightmode Terminal borders in lightmode changed

--- websource/src/components/Worlds.js
+++ websource/src/components/Worlds.js
@@ -4,62 +4,63 @@ import PlayerDropdown from './PlayerList/PlayerDropdown';
 import { prev_player } from './PlayersList';
 
 
-const Worlds = props => {
-	let worldsInfo = [];
-	const World = props => {
-		if (!worldsInfo.includes(props)) worldsInfo.push(props);
-		return (
-			<div className='world-grid'>
-				<div className='world' name={props.name} style={{ zIndex: props.pos }}>
-					<div className={props.status === 'unloaded' ? 'worldBG blurry' : 'worldBG'}></div>
-					<h2>{props.name}</h2>
-				</div>
-				<div className='worldCart_info'>
-					<table>
-						<tbody>
-							<tr>
-								<td>Status: </td>
-								<td id='wStatus'>{props.status}</td>
-							</tr>
-							<tr>
-								<td>Size: </td>
-								<td id='wSize'>{props.size}</td>
-							</tr>
-							<tr>
-								<td>Spawn: </td>
-								<td id='wSpawn'>{props.spawn}</td>
-							</tr>
-							<tr>
-								<td>Seed: </td>
-								<td id='wSeed'>{props.seed}</td>
-							</tr>
-							<tr>
-								<td>Textures: </td>
-								<td id='wTexturePack'>{props.texturepack}</td>
-							</tr>
-							<tr>
-								<td>Weather: </td>
-								<td id='wWeather'>{props.weather}</td>
-							</tr>
-						</tbody>
-					</table>
-				</div>
-				<div className='worldCart_players'>
-					<h3 id='wName'>Players</h3>
-					<ul className='plist' id='pList2'>
-						{ props.players.map((player) => {
-						   return <PlayerDropdown cwap={props.cwap}>{player}</PlayerDropdown>
-						})}
-					</ul>
-				</div>
+const World = props => {
+	const cwap = props.cwap;
+	return (
+		<div className='world-grid'>
+			<div className='world' name={props.name} style={{ zIndex: props.pos }}>
+				<div className={props.status === 'unloaded' ? 'worldBG blurry' : 'worldBG'}></div>
+				<h2>{props.name}</h2>
 			</div>
-		);
-	};
+			<div className='worldCart_info'>
+				<table>
+					<tbody>
+						<tr>
+							<td>Status: </td>
+							<td id='wStatus'>{props.status}</td>
+						</tr>
+						<tr>
+							<td>Size: </td>
+							<td id='wSize'>{props.size}</td>
+						</tr>
+						<tr>
+							<td>Spawn: </td>
+							<td id='wSpawn'>{props.spawn}</td>
+						</tr>
+						<tr>
+							<td>Seed: </td>
+							<td id='wSeed'>{props.seed}</td>
+						</tr>
+						<tr>
+							<td>Textures: </td>
+							<td id='wTexturePack'>{props.texturepack}</td>
+						</tr>
+						<tr>
+							<td>Weather: </td>
+							<td id='wWeather'>{props.weather}</td>
+						</tr>
+					</tbody>
+				</table>
+			</div>
+			<div className='worldCart_players'>
+				<h3 id='wName'>Players</h3>
+				<ul className='plist' id='pList2'>
+					{ props.players.map((player) => {
+					   return <PlayerDropdown cwap={cwap}>{player}</PlayerDropdown>
+					})}
+				</ul>
+			</div>
+		</div>
+	);
+};
 
+const Worlds = props => {
+	const cwap = props.cwap;
 	useEffect(() => {
 		const listElement = document.getElementById('pList2');
 		listElement.onscroll = (e) => prev_player && prev_player.classList.remove('show');
 	});
+
 	return (
 		<div className='worlds'>
 			<h3 className='worlds-header'>Worlds</h3>
@@ -67,7 +68,7 @@ const Worlds = props => {
 			<div className='worlds_list'>
 				{
 					props.worlds.map((world, pos) => {
-						return <World {...world} pos={pos+1} />
+						return <World cwap={props.cwap} {...world} pos={pos+1} />
 					})
 				}
 			</div>
