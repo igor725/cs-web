e51caa693e606c5795d47cf00fac3d970f47300e
igor725 <gvaldovigor@gmail.com>
2022-12-14 18:56:30 +0300

    Fix building on Linux

--- vars.sh
+++ vars.sh
@@ -1,3 +1,16 @@
+CFLAGS="$CFLAGS -I..";
+
+if [ -f "../cs-lua/src/luaitf.h" ]; then
+	echo "Lua interface connected";
+	CFLAGS="$CFLAGS -DCSWEB_USE_LUA";
+	. ../cs-lua/vars.sh;
+fi
+
+if [ -f "../cs-base/src/base_itf.h" ]; then
+	echo "Base interface connected";
+	CFLAGS="$CFLAGS -DCSWEB_USE_BASE";
+fi
+
 if [ $PLUGIN_INSTALL -eq 1 ]; then
 	if [ "$PLUGIN_ARGS" == "wbuild" ]; then
 		pushd "$ROOT/websource/";
